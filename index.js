b9af7f595cdcd3276d2ca420b0fb04fe39b07aaf
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2018-06-03 18:24:47 +0300

    fixed problems after clone, should compile fine now

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import MainMenu from './src/views/MainMenuView';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('discgolfscorecard', () => MainMenu);
